bd6fa48b401c801bc5fdaaca52a8e2e6b633cd62
knightbrilliance <alonsogutierrez94@gmail.com>
2017-06-29 15:26:49 -0400

    add new statistic based, outlier discriminating system

--- poi_id.py
+++ poi_id.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
 import sys
+import pandas as pd
 import pickle
+import pprint as pp
+import numpy as np
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -15,16 +18,15 @@ features_list = ['poi',
 				 'bonus', 
 				 'deferral_payments',
 				 'director_fees',
-				 'excecised_stock_options',
+				 'exercised_stock_options',
 				 'expenses',
 				 'from_messages',
 				 'from_poi_to_this_person',
-				 'from_person_to_this_poi',
+				 'from_this_person_to_poi',
 				 'loan_advances',
-				 'long_term_incetive',
+				 'long_term_incentive',
 				 'restricted_stock',
 				 'restricted_stock_deferred',
-				 'salary',
 				 'to_messages',
 				 'total_payments',
 				 'total_stock_value'] 
@@ -35,43 +37,87 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 ### Task 2: Remove outliers
 
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-features = ["salary", "bonus"]
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+features = features_list
 data = featureFormat(data_dict, features)
+df = pd.DataFrame(data, columns = features_list)
+
+dictionary = {}
+
+def greater_than_xstds(integer, std , mean, multiplier = 3):
+
+	difference = abs(integer - mean)
+	x = difference/std
+	if x >= multiplier:
+		return True
+	else:
+		return False
+
+
+def check_for_outliers(df, feature, xstd = 3):
+
+	std = df[feature].std()
+	mean = df[feature].mean()
+
+	dictionary[feature] = []
+	i = 0 
+
+	for value in df[feature]:
+		i += 1
+		if greater_than_xstds(value, std, mean, multiplier = xstd):
+			index_v = dictionary[feature].append(("position: " + str(i),
+												  "value: " + str(value),
+												  "mean: "+ str(mean),
+												  "std: "+ str(std)))
+		else:
+			pass
+
+	return dictionary
+
+outliers = 0
+
+for feature in features_list:
+	a = check_for_outliers(df, feature, xstd = 2.5)
+	if a:
+		outliers = a
+
+pp.pprint(outliers)
 
 ### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
-my_dataset = data_dict
 
-### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
-labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
+# ### Store to my_dataset for easy export below.
+# my_dataset = data_dict
 
-# Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+# ### Extract features and labels from dataset for local testing
+# data = featureFormat(my_dataset, features_list, sort_keys = True)
+# labels, features = targetFeatureSplit(data)
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+# ### Task 4: Try a varity of classifiers
+# ### Please name your classifier clf for easy export below.
+# ### Note that if you want to do PCA or other multi-stage operations,
+# ### you'll need to use Pipelines. For more info:
+# ### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+# # Provided to give you a starting point. Try a variety of classifiers.
+# from sklearn.naive_bayes import GaussianNB
+# clf = GaussianNB()
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
+# ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+# ### using our testing script. Check the tester.py script in the final project
+# ### folder for details on the evaluation method, especially the test_classifier
+# ### function. Because of the small size of the dataset, the script uses
+# ### stratified shuffle split cross validation. For more info: 
+# ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-dump_classifier_and_data(clf, my_dataset, features_list)
+# # Example starting point. Try investigating other evaluation techniques!
+# from sklearn.cross_validation import train_test_split
+# features_train, features_test, labels_train, labels_test = \
+#     train_test_split(features, labels, test_size=0.3, random_state=42)
+
+# ### Task 6: Dump your classifier, dataset, and features_list so anyone can
+# ### check your results. You do not need to change anything below, but make sure
+# ### that the version of poi_id.py that you submit can be run on its own and
+# ### generates the necessary .pkl files for validating your results.
+
+# dump_classifier_and_data(clf, my_dataset, features_list)
